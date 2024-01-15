81c31944c3ee8411b60062eb9fc0d677208ce708
Aaron Knapper <34011307+aknapper@users.noreply.github.com>
2024-01-14 21:47:38 -0500

    scd30 sensor test

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ setup(
     version='0.1.0',
     packages=find_packages(),  # Automatically discover and include all packages
     install_requires=[
-        'dash',
-        'pandas',
+        'dash==2.14.2',
+        'pandas==2.1.4',
+        'sensirion-shdlc-sensorbridge'
     ],
     entry_points={
         'console_scripts': [
